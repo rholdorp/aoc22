3ada28b20c22a26946429256b2109dfb3811738a
rholdorp <ralph.holdorp@gmail.com>
2022-12-16 22:15:36 +0100

    initial setup for d13

---
+++ d13/d13.js
@@ -0,0 +1,17 @@
+const readFileSync = require("fs").readFileSync;
+// const lines = readFileSync("input", { encoding: "utf-8" })
+const lines = readFileSync("example_input", { encoding: "utf-8" })
+  .replace(/\r/g, "")
+  .trim()
+  .split("\n");
+
+function part1() {
+  console.log("input file =", lines);
+}
+
+function part2() {
+  console.log("solution for part 2");
+}
+
+part1();
+part2();
